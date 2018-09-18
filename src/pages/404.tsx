d4eb71d19c89c18350bdd22783fd6040a63ec279
Ian Outterside <ian@komododigital.co.uk>
2018-09-18 10:25:47 +0100

    Finished starter

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../components/layout';
 
 export default () => (
-    <Layout>
-        <h1>#404 Page Not Found</h1>
-    </Layout>
+  <Layout>
+    <h1>#404 Page Not Found</h1>
+  </Layout>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
 import Layout from '../components/layout';
 
-export default () => (
-    <span>Hello World!</span>
-);
+export default () => <span>Hello World!</span>;
