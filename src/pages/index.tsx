df60334f55bfaeec7f372cebd95366514a639eb9
Ian Outterside <ian@komododigital.co.uk>
2018-09-18 09:28:09 +0100

    Working gatsby starter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 
 export default () => (
-    <Layout>
-        <Typography variant="headline">Hello World!</Typography>
-    </Layout>
+    <span>Hello World!</span>
 )
