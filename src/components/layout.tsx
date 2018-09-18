8f08a54f7862cb2fe6d8e2ba118d2b785ca292a5
Ian Outterside <ian@komododigital.co.uk>
2018-09-18 10:15:02 +0100

    Reformat based on tslint / prettier styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,26 +8,37 @@ interface LayoutProps {
 const Layout: React.SFC<LayoutProps> = ({ children }) => {
   const siteName = 'my Site name';
   return (
-        <div>
-            <StaticQuery
-                query={ graphql `query HeaderQuery {
-                    site { siteMetadata { name, title, description } }
-                }`}
-                render={ (data) => (
-                    <Helmet defaultTitle={data.site.siteMetadata.title}>
-                        <html lang="en" />
-                        <meta property="og:type" content="website" />
-                        <meta property="og:site_name" content={data.site.siteMetadata.name} />
-                        <meta name="description" content={data.site.siteMetadata.description} />
-                        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto: 300,400,500" />
-                        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-                    </Helmet>
-                )}
+    <div>
+      <StaticQuery
+        query={graphql`
+          query HeaderQuery {
+            site {
+              siteMetadata {
+                name
+                title
+                description
+              }
+            }
+          }
+        `}
+        render={data => (
+          <Helmet defaultTitle={data.site.siteMetadata.title}>
+            <html lang="en" />
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content={data.site.siteMetadata.name} />
+            <meta name="description" content={data.site.siteMetadata.description} />
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Roboto: 300,400,500"
             />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          </Helmet>
+        )}
+      />
 
-            {children}
-        </div>
-    );
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
