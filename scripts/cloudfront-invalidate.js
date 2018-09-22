72bbb67454f27920184a1cec6700367ad362fb27
Ian Outterside <ianoshorty@gmail.com>
2018-09-22 14:39:30 +0100

    Explicitly set AWS credentials within script

--- scripts/cloudfront-invalidate.js
+++ scripts/cloudfront-invalidate.js
@@ -11,17 +11,28 @@ const moment = require('moment');
 
 // CloudFront requires a unique reference for every invalidation
 const reference = `${randomstring.generate(16)}-${moment().format('X')}`;
+const access_key_id = process.env.AWS_ACCESS_KEY_ID;
+const secret_access_key = process.env.AWS_SECRET_ACCESS_KEY;
 
-const cloudfront = new AWS.CloudFront();
+if (!access_key_id || !secret_access_key) {
+  console.error('Unable to fetch AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY from environment variables.');
+  process.exit(1);
+}
+
+const cloudfront = new AWS.CloudFront({
+  accessKeyId: access_key_id.trim(),
+  secretAccessKey: secret_access_key.trim(),
+});
+
 const distributionId = process.env.DISTRIBUTION_ID; /* env */
 
 if (!distributionId) {
-    console.error('Unable to fetch DISTRIBUTION_ID from environment variables.')
-    process.exit(1);
+  console.error('Unable to fetch DISTRIBUTION_ID from environment variables.');
+  process.exit(1);
 }
 
 const params = {
-  DistributionId: distributionId,
+  DistributionId: distributionId.trim(),
   InvalidationBatch: {
     CallerReference: reference,
     Paths: {
