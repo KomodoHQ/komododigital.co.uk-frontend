5f9f680687d8606e8ebcc9680471248ee39fbccc
Ian Outterside <ianoshorty@gmail.com>
2018-09-22 21:29:52 +0100

    Fixing tests for graphql in blog

--- test/components/blogsummaryview.test.jsx
+++ test/components/blogsummaryview.test.jsx
@@ -3,7 +3,11 @@ import BlogSummaryView from '../../src/components/blogsummaryview.tsx';
 import renderer from 'react-test-renderer';
 
 test('BlogSummaryView element is rendered', () => {
-  const component = renderer.create(<BlogSummaryView />);
+  const data = {
+    imageSource: 'https://www.komododigital.co.uk/static/media/logo.50a4b652.svg',
+    date: "2018-09-22"
+  };
+  const component = renderer.create(<BlogSummaryView data={data} />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
