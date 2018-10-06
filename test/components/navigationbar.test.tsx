e04c968735171434f9051574b97ac2e5ff43c4d9
Ian Outterside <ianoshorty@gmail.com>
2018-10-06 21:49:35 +0100

    Add support for sass/css imports in jest via mocks

--- test/components/navigationbar.test.tsx
+++ test/components/navigationbar.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-//import NavigationBar from '../../src/components/navigationbar';
+import NavigationBar from '../../src/components/navigationbar';
 import renderer from 'react-test-renderer';
 
 test('NavigationBar element is rendered', () => {
   const data = {
   };
-  // const component = renderer.create(<NavigationBar data={data} />);
-  // let tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
+  const component = renderer.create(<NavigationBar data={data} />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
