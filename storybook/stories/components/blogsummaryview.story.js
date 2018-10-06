5237732e9f7cab4a172285d115fe4ea26e897000
Chris Neale <chris@komododigital.co.uk>
2018-10-06 13:26:07 +0100

    Move existing stories in to component directory

--- storybook/stories/components/blogsummaryview.story.js
+++ storybook/stories/components/blogsummaryview.story.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
-import BlogSummaryView from '../../src/components/blogsummaryview.tsx'
+import BlogSummaryView from '../../../src/components/blogsummaryview.tsx'
 
 const data = {
   title: 'Test Post',
--- storybook/stories/components/titletext.story.js
+++ storybook/stories/components/titletext.story.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import TitleText from '../../src/components/titletext'
+import TitleText from '../../../src/components/titletext'
 
 storiesOf('TitleText', module).add(
   'with text',
