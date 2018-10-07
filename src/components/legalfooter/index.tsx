57c25a754499bbd752631d8a87d5e1828cdf6d21
Chris Neale <chris@komododigital.co.uk>
2018-10-07 16:35:18 +0100

    Footers init

--- src/components/legalfooter/index.tsx
+++ src/components/legalfooter/index.tsx
@@ -1,4 +1,6 @@
 import React, { ReactNode } from 'react';
+import { Link } from '@reach/router';
+import "./legalfooter.css";
 
 interface Props {
   data: any;
@@ -6,9 +8,18 @@ interface Props {
 
 const LegalFooter: React.SFC<Props> = ({ data }) => {
   return (
-    <div>
-        LegalFooter
-    </div>
+    <footer>
+        <div>
+          Komodo Digital Copyright &copy; 2018
+          <Link to="/">Privacy Policy</Link>
+          |
+          <Link to="/">Terms and Conditions</Link>
+        </div>
+        <div>
+          Get in touch on:
+          <Link to="tel:0191 228 6555">0191 228 6555</Link>
+        </div>
+    </footer>
   );
 };
 
