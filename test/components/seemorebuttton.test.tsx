8a2ec37c1bc573edf2e1d85df537c53886d48289
Ian Outterside <ianoshorty@gmail.com>
2018-10-12 19:38:02 +0100

    Fixing typescript errors

--- test/components/seemorebuttton.test.tsx
+++ test/components/seemorebuttton.test.tsx
@@ -3,9 +3,7 @@ import SeeMoreButton from '../../src/components/seemorebutton';
 import renderer from 'react-test-renderer';
 
 test('SeeMoreButton element is rendered', () => {
-  const data = {
-  };
-  const component = renderer.create(<SeeMoreButton data={data} />);
-  let tree = component.toJSON();
+  const component = renderer.create(<SeeMoreButton title={'test'} />);
+  const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
