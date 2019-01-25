62757d0c640550f32682e9a93e4d8d6a6155825e
Ian Outterside <ianoshorty@gmail.com>
2019-01-25 20:52:17 +0000

    Working demo of live editing content

--- src/components/contentsection/index.tsx
+++ src/components/contentsection/index.tsx
@@ -6,8 +6,91 @@
  * NPM Dependencies
  */
 import React, { ReactNode } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import Img from 'gatsby-image';
 import './content-section.css';
+
+import { Editor } from 'slate-react';
+import Plain from 'slate-plain-serializer';
+import Prism from 'prismjs';
+import Html from 'slate-html-serializer';
+
+// eslint-disable-next-line
+Prism.languages.markdown=Prism.languages.extend("markup",{}),Prism.languages.insertBefore("markdown","prolog",{blockquote:{pattern:/^>(?:[\t ]*>)*/m,alias:"punctuation"},code:[{pattern:/^(?: {4}|\t).+/m,alias:"keyword"},{pattern:/``.+?``|`[^`\n]+`/,alias:"keyword"}],title:[{pattern:/\w+.*(?:\r?\n|\r)(?:==+|--+)/,alias:"important",inside:{punctuation:/==+$|--+$/}},{pattern:/(^\s*)#+.+/m,lookbehind:!0,alias:"important",inside:{punctuation:/^#+|#+$/}}],hr:{pattern:/(^\s*)([*-])([\t ]*\2){2,}(?=\s*$)/m,lookbehind:!0,alias:"punctuation"},list:{pattern:/(^\s*)(?:[*+-]|\d+\.)(?=[\t ].)/m,lookbehind:!0,alias:"punctuation"},"url-reference":{pattern:/!?\[[^\]]+\]:[\t ]+(?:\S+|<(?:\\.|[^>\\])+>)(?:[\t ]+(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\)))?/,inside:{variable:{pattern:/^(!?\[)[^\]]+/,lookbehind:!0},string:/(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\))$/,punctuation:/^[\[\]!:]|[<>]/},alias:"url"},bold:{pattern:/(^|[^\\])(\*\*|__)(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,lookbehind:!0,inside:{punctuation:/^\*\*|^__|\*\*$|__$/}},italic:{pattern:/(^|[^\\])([*_])(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,lookbehind:!0,inside:{punctuation:/^[*_]|[*_]$/}},url:{pattern:/!?\[[^\]]+\](?:\([^\s)]+(?:[\t ]+"(?:\\.|[^"\\])*")?\)| ?\[[^\]\n]*\])/,inside:{variable:{pattern:/(!?\[)[^\]]+(?=\]$)/,lookbehind:!0},string:{pattern:/"(?:\\.|[^"\\])*"(?=\)$)/}}}}),Prism.languages.markdown.bold.inside.url=Prism.util.clone(Prism.languages.markdown.url),Prism.languages.markdown.italic.inside.url=Prism.util.clone(Prism.languages.markdown.url),Prism.languages.markdown.bold.inside.italic=Prism.util.clone(Prism.languages.markdown.italic),Prism.languages.markdown.italic.inside.bold=Prism.util.clone(Prism.languages.markdown.bold); // prettier-ignore
+
+const BLOCK_TAGS = {
+  blockquote: 'quote',
+  p: 'paragraph',
+  pre: 'code',
+}
+
+// Add a dictionary of mark tags.
+const MARK_TAGS = {
+  em: 'italic',
+  strong: 'bold',
+  u: 'underline',
+}
+
+const rules = [
+  {
+    deserialize(el, next) {
+      const type = BLOCK_TAGS[el.tagName.toLowerCase()]
+      if (type) {
+        return {
+          object: 'block',
+          type: type,
+          data: {
+            className: el.getAttribute('class'),
+          },
+          nodes: next(el.childNodes),
+        }
+      }
+    },
+    serialize(obj, children) {
+      if (obj.object == 'block') {
+        switch (obj.type) {
+          case 'code':
+            return (
+              <pre>
+                <code>{children}</code>
+              </pre>
+            )
+          case 'paragraph':
+            return <p className={obj.data.get('className')}>{children}</p>
+          case 'quote':
+            return <blockquote>{children}</blockquote>
+        }
+      }
+    },
+  },
+  // Add a new rule that handles marks...
+  {
+    deserialize(el, next) {
+      const type = MARK_TAGS[el.tagName.toLowerCase()]
+      if (type) {
+        return {
+          object: 'mark',
+          type: type,
+          nodes: next(el.childNodes),
+        }
+      }
+    },
+    serialize(obj, children) {
+      if (obj.object == 'mark') {
+        switch (obj.type) {
+          case 'bold':
+            return <strong>{children}</strong>
+          case 'italic':
+            return <em>{children}</em>
+          case 'underline':
+            return <u>{children}</u>
+        }
+      }
+    },
+  },
+]
+
+const html = new Html({ rules })
 
 /**
  * Local dependencies
@@ -15,7 +98,7 @@ import './content-section.css';
 
 /**
  * Props for the content section component
- * 
+ *
  * data
  */
 interface Props {
@@ -27,42 +110,259 @@ interface Props {
   children: ReactNode;
   coverimage?: any;
 }
+class ContentSection extends React.Component<Props> {
+  state = {
+    subtitleValue: this.props.subtitle
+      ? Plain.deserialize(this.props.subtitle)
+      : Plain.deserialize(''),
+    titleValue: this.props.title ? Plain.deserialize(this.props.title) : Plain.deserialize(''),
+    bodyValue: html.deserialize(ReactDOMServer.renderToString(this.props.children)),
+    editing: true,
+  };
 
-/**
- * 
- * Using SFC (Stateless Functional Component) because a content section doesn't need to maintain any state of its own.
- * 
- * @param data
- */
-const ContentSection: React.SFC<Props> = ({ title = 'TITLE HERE', subtitle= null, invert, background='none', className='', children, coverimage=null }) => {
+  // On change, update the app's React state with the new editor value.
+  onChange = ({ value }) => {
+    this.setState({ value });
+  };
 
-  const subtitleEl = subtitle ? (<span>{subtitle}</span>) : null;
-  const titleEl = title ? (<h2>{title}</h2>) : null;
-  const invertedClassname = invert ? "invert" : "";
+  /**
+   * Render a Slate mark.
+   *
+   * @param {Object} props
+   * @param {Editor} editor
+   * @param {Function} next
+   * @return {Element}
+   */
 
-  const style = {
-    background: background
+  renderMark = (props, editor, next) => {
+    const { children, mark, attributes } = props;
+
+    switch (mark.type) {
+      case 'bold':
+        return <strong {...attributes}>{children}</strong>;
+
+      case 'code':
+        return <code {...attributes}>{children}</code>;
+
+      case 'italic':
+        return <em {...attributes}>{children}</em>;
+
+      case 'underlined':
+        return <u {...attributes}>{children}</u>;
+
+      case 'title': {
+        return (
+          <span
+            {...attributes}
+            style={{
+              fontWeight: 'bold',
+              fontSize: '20px',
+              margin: '20px 0 10px 0',
+              display: 'inline-block',
+            }}
+          >
+            {children}
+          </span>
+        );
+      }
+
+      case 'punctuation': {
+        return (
+          <span {...attributes} style={{ opacity: 0.2 }}>
+            {children}
+          </span>
+        );
+      }
+
+      case 'list': {
+        return (
+          <span
+            {...attributes}
+            style={{
+              paddingLeft: '10px',
+              lineHeight: '10px',
+              fontSize: '20px',
+            }}
+          >
+            {children}
+          </span>
+        );
+      }
+
+      case 'hr': {
+        return (
+          <span
+            {...attributes}
+            style={{
+              borderBottom: '2px solid #000',
+              display: 'block',
+              opacity: 0.2,
+            }}
+          >
+            {children}
+          </span>
+        );
+      }
+
+      default: {
+        return next();
+      }
+    }
+  };
+
+  /**
+   * Define a decorator for markdown styles.
+   *
+   * @param {Node} node
+   * @param {Function} next
+   * @return {Array}
+   */
+
+  decorateNode(node, editor, next) {
+    const others = next() || [];
+    if (node.object !== 'block') return others;
+
+    const string = node.text;
+    const texts = node.getTexts().toArray();
+    const grammar = Prism.languages.markdown;
+    const tokens = Prism.tokenize(string, grammar);
+    const decorations = [];
+    let startText = texts.shift();
+    let endText = startText;
+    let startOffset = 0;
+    let endOffset = 0;
+    let start = 0;
+
+    function getLength(token) {
+      if (typeof token == 'string') {
+        return token.length;
+      } else if (typeof token.content == 'string') {
+        return token.content.length;
+      } else {
+        return token.content.reduce((l, t) => l + getLength(t), 0);
+      }
+    }
+
+    for (const token of tokens) {
+      startText = endText;
+      startOffset = endOffset;
+
+      const length = getLength(token);
+      const end = start + length;
+
+      let available = startText.text.length - startOffset;
+      let remaining = length;
+
+      endOffset = startOffset + remaining;
+
+      while (available < remaining) {
+        endText = texts.shift();
+        remaining = length - available;
+        available = endText.text.length;
+        endOffset = remaining;
+      }
+
+      if (typeof token != 'string') {
+        const dec = {
+          anchor: {
+            key: startText.key,
+            offset: startOffset,
+          },
+          focus: {
+            key: endText.key,
+            offset: endOffset,
+          },
+          mark: {
+            type: token.type,
+          },
+        };
+
+        decorations.push(dec);
+      }
+
+      start = end;
+    }
+
+    return [...others, ...decorations];
   }
 
-  let coverImage;
-  if (coverimage!==null) {
-    coverImage = <Img fluid={coverimage.childImageSharp.fluid} />;
-    // style.paddingBottom = "0";
-  } else {
-    coverImage = null;
-  }
+  render() {
+    const {
+      title = 'TITLE HERE',
+      subtitle = null,
+      invert,
+      background = 'none',
+      className = '',
+      children,
+      coverimage = null,
+    } = this.props;
 
-  return (
-    <div className={`komodoGridWrapper cs-wrapper ${invertedClassname} ${className}`} style={style}>
-      <div className={`Content-Section`}>
-        {subtitleEl}
-        {titleEl}
-        <div>{children}</div>
+    const subtitleEl = subtitle ? <span>{subtitle}</span> : null;
+    const titleEl = title ? <h2>{title}</h2> : null;
+    const invertedClassname = invert ? 'invert' : '';
+
+    const style = {
+      background,
+    };
+
+    let coverImage;
+    if (coverimage !== null) {
+      coverImage = <Img fluid={coverimage.childImageSharp.fluid} />;
+    } else {
+      coverImage = null;
+    }
+
+    console.log(this.props.children);
+
+    return (
+      <div
+        className={`komodoGridWrapper cs-wrapper ${invertedClassname} ${className}`}
+        style={style}
+      >
+        <div className={`Content-Section`}>
+          {this.state.editing && (
+            <span>
+              <Editor
+                placeholder="Write some markdown..."
+                defaultValue={this.state.subtitleValue}
+                renderMark={this.renderMark}
+                decorateNode={this.decorateNode}
+              />
+            </span>
+          )}
+
+          {!this.state.editing && subtitleEl}
+
+          {this.state.editing && (
+            <h2>
+              <Editor
+                placeholder="Write some markdown..."
+                defaultValue={this.state.titleValue}
+                renderMark={this.renderMark}
+                decorateNode={this.decorateNode}
+              />
+            </h2>
+          )}
+
+          {!this.state.editing && titleEl}
+
+          {this.state.editing && (
+            <div>
+              <Editor
+                placeholder="Write some markdown..."
+                defaultValue={this.state.bodyValue}
+                renderMark={this.renderMark}
+                decorateNode={this.decorateNode}
+              />
+            </div>
+          )}
+
+          {!this.state.editing && <div>{children}</div>}
+        </div>
+        {coverImage}
       </div>
-      {coverImage}
-    </div>
-
-  );
-};
+    );
+  }
+}
 
 export default ContentSection;
--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -9,13 +9,13 @@ import GridItem from '../components/griditem';
 import StandardItem from '../components/standarditem';
 import ContentSection from '../components/contentsection';
 import ContentImage from '../components/contentimage';
-import BlogGrid from "../components/bloggrid";
+import BlogGrid from '../components/bloggrid';
 import BlogPost from '../components/blogpost';
 import SeeMoreButton from '../components/seemorebutton';
 import ContactSection from '../components/contactsection';
 import ContactForm from '../components/contactform';
 
-import images from "../utils/images";
+import images from '../utils/images';
 
 const renderAst = new rehypeReact({
   createElement: React.createElement,
@@ -29,7 +29,7 @@ interface Insight {
   node: any;
 }
 
- interface Service {
+interface Service {
   title: string;
   image: any;
   htmlAst: any;
@@ -59,87 +59,126 @@ interface IndexPageProps {
   data?: any;
 }
 
-export default (props: IndexPageProps) => {
-  return (
-    <Layout data={props.data} >
-      <ContentSection title={props.title} subtitle={props.subtitle} invert background="#000000" className="topPaddingLarge bottomPaddingLarge">
-        {renderAst(props.intro)}
-      </ContentSection>
-      <ContentSection title="About Us" className="topPaddingLarge bottomPaddingsmall">
-        {renderAst(props.aboutUsIntro)}
-      </ContentSection>
-      <ContentImage>
-        <object type="image/svg+xml" data={require("../content/images/illustration@1.5x.svg")} />
-      </ContentImage>
-      <ContentSection title="Approach" className="topPaddingLarge bottomPaddingSmall">
-        {renderAst(props.approachIntro)}
-      </ContentSection>
-      <QuadGrid>
-        {props.services.map((service) => {
+export default class HomePage extends React.Component<IndexPageProps> {
+  render() {
+    const props = this.props;
+
+    return (
+      <Layout data={props.data}>
+        <ContentSection
+          title={props.title}
+          subtitle={props.subtitle}
+          invert
+          background="#000000"
+          className="topPaddingLarge bottomPaddingLarge"
+        >
+          {renderAst(props.intro)}
+        </ContentSection>
+        <ContentSection title="About Us" className="topPaddingLarge bottomPaddingsmall">
+          {renderAst(props.aboutUsIntro)}
+        </ContentSection>
+        <ContentImage>
+          <object type="image/svg+xml" data={require('../content/images/illustration@1.5x.svg')} />
+        </ContentImage>
+        <ContentSection title="Approach" className="topPaddingLarge bottomPaddingSmall">
+          {renderAst(props.approachIntro)}
+        </ContentSection>
+        <QuadGrid>
+          {props.services.map((service) => {
+            return (
+              <GridItem
+                key={service.title}
+                title={service.title}
+                image={images[`images/approach/${service.title}@1.5x.svg`]}
+              >
+                {renderAst(service.htmlAst)}
+              </GridItem>
+            );
+          })}
+        </QuadGrid>
+        <ContentSection title="Case Studies" className="topPaddingLarge bottomPaddingSmall">
+          {renderAst(props.caseStudiesIntro)}
+        </ContentSection>
+        {props.caseStudies.map((study) => {
           return (
-            <GridItem key={service.title} title={service.title} image={images[`images/approach/${service.title}@1.5x.svg`]}>
-              {renderAst(service.htmlAst)}
-            </GridItem>
+            <CaseStudy
+              key={study.title}
+              subtitle={study.subtitle}
+              title={study.title}
+              link={study.link}
+              image={study.csimage}
+            >
+              {renderAst(study.htmlAst)}
+            </CaseStudy>
           );
         })}
-      </QuadGrid>
-      <ContentSection title="Case Studies" className="topPaddingLarge bottomPaddingSmall">
-        {renderAst(props.caseStudiesIntro)}
-      </ContentSection>
-      {props.caseStudies.map((study) => {
-        return (
-          <CaseStudy
-            key={study.title}
-            subtitle={study.subtitle}
-            title={study.title}
-            link={study.link}
-            image={study.csimage}
-          >
-            {renderAst(study.htmlAst)}
-          </CaseStudy>
-        );
-      })}
-      <SeeMoreButton title="See More Work" />
-      <ContentSection title="Client Portfolio" className="topPaddingMedium">
-        {renderAst(props.clientPortfoliosIntro)}
-      </ContentSection>
-      <PortfolioGrid>
-
-        <StandardItem imgsrc={props.data.client_cop} title="College of Policing" link="/About" />
-        <StandardItem imgsrc={props.data.client_digitalbarriers} title="Digital Barriers" link="/About" />
-        <StandardItem imgsrc={props.data.client_elanders} title="eLanders" link="/About" />
-        <StandardItem imgsrc={props.data.client_gichd} title="GICHD" link="/About" />
-        <StandardItem imgsrc={props.data.client_htl} title="HTL" link="/About" />
-        <StandardItem imgsrc={props.data.client_isg} title="ISG" link="/About" />
-        <StandardItem imgsrc={props.data.client_itv} title="ITV" link="/About" />
-        <StandardItem imgsrc={props.data.client_legrand} title="LeGrand" link="/About" />
-        <StandardItem imgsrc={props.data.client_spacegroup} title="Space Group" link="/About" />
-        <StandardItem imgsrc={props.data.client_newcastleuni} title="Newcastle University" link="/About" />
-        <StandardItem imgsrc={props.data.client_northumbriapolice} title="Northumbria Police" link="/About" />
-        <StandardItem imgsrc={props.data.client_northumbriauni} title="Northumbria University" link="/About" />
-        <StandardItem imgsrc={props.data.client_onward} title="Onward" link="/About" />
-        <StandardItem imgsrc={props.data.client_orchard} title="Orchard" link="/About" />
-        <StandardItem imgsrc={props.data.client_streetstream} title="Streetstream" link="/About" />
-        <StandardItem imgsrc={props.data.client_thingco} title="ThingCo" link="/About" />
-
-      </PortfolioGrid>
-      <ContentSection title="Insights" className="topPaddingMedium">
-        {renderAst(props.insightsIntro)}
-      </ContentSection>
-      <BlogGrid>
-      {props.insights.map((insight) => {
-        return <BlogPost key={insight.node.title} slug={insight.node.slug} title={insight.node.title} image={insight.node.imageSource} />;
-      })}
-      </BlogGrid>
-      <SeeMoreButton title="See More Insights" link={"/blog-list"} />
-      <ContentSection title="Contact Us" background="#FFFFFF" className="topPaddingMedium">
-        {renderAst(props.contactsIntro)}
-      </ContentSection>
-      <ContactSection>
-        <VCard person="Armin" avatar={props.data.avatar} />
-        <VCard person="Phoebe" avatar={props.data.avatar} />
-        <ContactForm />
-      </ContactSection>
-    </Layout>
-  );
-};
+        <SeeMoreButton title="See More Work" />
+        <ContentSection title="Client Portfolio" className="topPaddingMedium">
+          {renderAst(props.clientPortfoliosIntro)}
+        </ContentSection>
+        <PortfolioGrid>
+          <StandardItem imgsrc={props.data.client_cop} title="College of Policing" link="/About" />
+          <StandardItem
+            imgsrc={props.data.client_digitalbarriers}
+            title="Digital Barriers"
+            link="/About"
+          />
+          <StandardItem imgsrc={props.data.client_elanders} title="eLanders" link="/About" />
+          <StandardItem imgsrc={props.data.client_gichd} title="GICHD" link="/About" />
+          <StandardItem imgsrc={props.data.client_htl} title="HTL" link="/About" />
+          <StandardItem imgsrc={props.data.client_isg} title="ISG" link="/About" />
+          <StandardItem imgsrc={props.data.client_itv} title="ITV" link="/About" />
+          <StandardItem imgsrc={props.data.client_legrand} title="LeGrand" link="/About" />
+          <StandardItem imgsrc={props.data.client_spacegroup} title="Space Group" link="/About" />
+          <StandardItem
+            imgsrc={props.data.client_newcastleuni}
+            title="Newcastle University"
+            link="/About"
+          />
+          <StandardItem
+            imgsrc={props.data.client_northumbriapolice}
+            title="Northumbria Police"
+            link="/About"
+          />
+          <StandardItem
+            imgsrc={props.data.client_northumbriauni}
+            title="Northumbria University"
+            link="/About"
+          />
+          <StandardItem imgsrc={props.data.client_onward} title="Onward" link="/About" />
+          <StandardItem imgsrc={props.data.client_orchard} title="Orchard" link="/About" />
+          <StandardItem
+            imgsrc={props.data.client_streetstream}
+            title="Streetstream"
+            link="/About"
+          />
+          <StandardItem imgsrc={props.data.client_thingco} title="ThingCo" link="/About" />
+        </PortfolioGrid>
+        <ContentSection title="Insights" className="topPaddingMedium">
+          {renderAst(props.insightsIntro)}
+        </ContentSection>
+        <BlogGrid>
+          {props.insights.map((insight) => {
+            return (
+              <BlogPost
+                key={insight.node.title}
+                slug={insight.node.slug}
+                title={insight.node.title}
+                image={insight.node.imageSource}
+              />
+            );
+          })}
+        </BlogGrid>
+        <SeeMoreButton title="See More Insights" link={'/blog-list'} />
+        <ContentSection title="Contact Us" background="#FFFFFF" className="topPaddingMedium">
+          {renderAst(props.contactsIntro)}
+        </ContentSection>
+        <ContactSection>
+          <VCard person="Armin" avatar={props.data.avatar} />
+          <VCard person="Phoebe" avatar={props.data.avatar} />
+          <ContactForm />
+        </ContactSection>
+      </Layout>
+    );
+  }
+}
