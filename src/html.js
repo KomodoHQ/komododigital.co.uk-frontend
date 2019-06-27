a69b98610a53606448743f11756716d98e0182a3
Jason Hall <jason.hall@komododigital.co.uk>
2019-06-27 15:37:40 +0100

    Add hotjar tracking script

--- src/html.js
+++ src/html.js
@@ -42,6 +42,17 @@ export default class HTML extends React.Component {
           />
           <script type="text/javascript" src="https://secure.leadforensics.com/js/159776.js" ></script>
           <noscript><img alt="" src="https://secure.leadforensics.com/159776.png" style={{display: "none"}} /></noscript>
+          <!-- Hotjar Tracking Code for https://www.komododigital.co.uk/ -->
+          <script>
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:620185,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          </script>
         </body>
       </html>
     );
